e05cccc524968a6a2103fc7e4a54aa44171479f1
Kevin Gysberg <kevin@gysberg.me>
2019-02-19 19:53:11 -0600

    - Reorganized loading of configuration     - Loads in priority order: NgrokConfig Profile, NgrokOptions.ApplicationHttpUrl, IServerFeatures     - If no app urls found, an exception is thrown

--- src/NgrokAspNetCore.Lib/NgrokAspNetCoreExtensions.cs
+++ src/NgrokAspNetCore.Lib/NgrokAspNetCoreExtensions.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using NgrokAspNetCore.Exceptions;
 using NgrokExtensions;
 using System;
 using System.Collections.Generic;
@@ -27,14 +28,15 @@ namespace NgrokAspNetCore
 
 			services.AddLogging();
 		}
+
 		/// <summary>
 		/// Start ngrok.exe with configured options
 		/// </summary>
 		/// <exception cref="NgrokStartFailedException">Throws when ngrok failed to start</exception>
 		/// <exception cref="NgrokUnsupportedException">Throws when ngrok is not supported on the OS and architecture</exception>
 		/// <exception cref="NgrokNotFoundException">Throws when ngrok is not found and is unable to be downloaded automatically</exception>
+		/// /// <exception cref="NgrokHostNotFoundException">Throws when a local address to tunnel to is not found and not configured</exception>
 		/// <param name="host"></param>
-		/// <param name="options"></param>
 		/// <returns></returns>
 		public static async Task<IEnumerable<Tunnel>> StartNgrokAsync(this IWebHost host)
 		{
@@ -46,9 +48,7 @@ namespace NgrokAspNetCore
 			// Start Ngrok
 			var ngrokClient = services.GetRequiredService<NgrokLocalApiClient>();
 
-			var tunnels = await ngrokClient.StartTunnelsAsync(options.NgrokPath);
-
-			return tunnels;
+			return await ngrokClient.StartTunnelsAsync(options.NgrokPath);
 		}
 
 		private static async Task<NgrokOptions> ConfigureOptions(IWebHost host)
@@ -56,17 +56,36 @@ namespace NgrokAspNetCore
 			var services = host.Services;
 			var options = services.GetRequiredService<NgrokOptions>();
 
-			// Set address automatically if not provided or invalid
-			var addresses = (host.ServerFeatures[typeof(IServerAddressesFeature)] as IServerAddressesFeature)?.Addresses;
-			if (!string.IsNullOrWhiteSpace(options.ApplicationHttpUrl) || !Uri.TryCreate(options.ApplicationHttpUrl, UriKind.Absolute, out Uri uri))
-			{
-				options.ApplicationHttpUrl = addresses.FirstOrDefault(a => a.StartsWith("http://")) ?? addresses.FirstOrDefault();
-			}
-
-			// Ensure ngrok is installed and set path
+			// Ensure ngrok is installed and set path in options. Throws if not found and not downloaded
 			var ngrokDownloader = services.GetRequiredService<NgrokDownloader>();
 			var ngrokFullPath = await ngrokDownloader.EnsureNgrokInstalled(options);
 			options.NgrokPath = ngrokFullPath;
+
+			// If config is driven off an NgrokConfig, short circuit
+			if (options.NgrokConfigProfile.HasValue())
+			{
+				return options;
+			}
+
+			// Check if an app url was provided and if it is valid. If so, short circuit
+			var optionsUrlValid = options.ApplicationHttpUrl.HasValue() && Uri.TryCreate(options.ApplicationHttpUrl, UriKind.Absolute, out Uri uri);
+			if (optionsUrlValid)
+			{
+				return options;
+			}
+
+			// Since the app address wasn't specified anywhere else, grab it from the server features. If null, throw exception
+			var addresses = (host.ServerFeatures[typeof(IServerAddressesFeature)] as IServerAddressesFeature)?.Addresses;
+			addresses = addresses ?? throw new NgrokHostNotFoundException();
+
+			// Get first http address from address list. Throw exception if none found
+			// Note: Make this configurable if we eventually integrate TLS termination with Core HTTPS/HSTS
+			var localAddress = addresses.FirstOrDefault(a => a.StartsWith("http://"));
+			if (!localAddress.HasValue())
+			{
+				throw new NgrokHostNotFoundException();
+			}
+			options.ApplicationHttpUrl = localAddress;
 
 			return options;
 		}
