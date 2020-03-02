bb49f5776afd0e7318626f629d4b2a5de8fccf45
Kevin Gysberg <kevin@gysberg.com>
2020-03-02 05:48:11 -0600

    Added unit testing project back for Ngrok.AspNetCore

---
+++ test/Ngrok.AspNetCore.UnitTests/NgrokLogExtensionsTests.cs
@@ -0,0 +1,45 @@
+using NgrokAspNetCore.Lib.Extensions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Ngrok.AspNetCore.UnitTests
+{
+	public class NgrokLogExtensionsTests
+	{
+		[Fact]
+		public void ParseStartTunnel()
+		{
+			var input = "t=2020-03-01T23:36:39-0600 lvl=info msg=\"started tunnel\" obj=tunnels name=test_tunnel addr=http://localhost:425 url=https://12030abc.ngrok.io";
+			var expectedResult = new Dictionary<string, string>()
+			{
+				{ "t", "2020-03-01T23:36:39-0600" },
+				{ "lvl", "info" },
+				{ "msg", "started tunnel" },
+				{ "obj", "tunnels" },
+				{ "name", "test_tunnel" },
+				{ "addr", "http://localhost:425" },
+				{ "url", "https://12030abc.ngrok.io" }
+			};
+			var result = NgrokLogExtensions.ParseLogData(input);
+
+			Assert.Equal(expectedResult, result);
+		}
+
+		[Fact]
+		public void ParseMisc()
+		{
+			var input = "t=2020-03-02T00:44:26-0600 lvl=info msg=\"no configuration paths supplied\"";
+			var expectedResult = new Dictionary<string, string>()
+			{
+				{ "t", "2020-03-02T00:44:26-0600" },
+				{ "lvl", "info" },
+				{ "msg", "no configuration paths supplied" }
+			};
+
+			var result = NgrokLogExtensions.ParseLogData(input);
+
+			Assert.Equal(expectedResult, result);
+		}
+	}
+}
