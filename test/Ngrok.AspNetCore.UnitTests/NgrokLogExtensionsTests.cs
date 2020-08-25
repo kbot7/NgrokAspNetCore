a6ae9d1040de95455063b5c05bc73326f710ccaf
Jaci Kust <jaci.kust@protonmail.com>
2020-08-25 12:30:13 -0600

    Renamed Test Methods to be more descriptive

--- test/Ngrok.AspNetCore.UnitTests/NgrokLogExtensionsTests.cs
+++ test/Ngrok.AspNetCore.UnitTests/NgrokLogExtensionsTests.cs
@@ -8,9 +8,9 @@ namespace Ngrok.AspNetCore.UnitTests
 	public class NgrokLogExtensionsTests
 	{
 		[Fact]
-		public void ParseStartTunnel()
+		public void ParseTunnelStartLogDataIntoDictionary()
 		{
-			var input = "t=2020-03-01T23:36:39-0600 lvl=info msg=\"started tunnel\" obj=tunnels name=test_tunnel addr=http://localhost:425 url=https://12030abc.ngrok.io";
+			var logData = "t=2020-03-01T23:36:39-0600 lvl=info msg=\"started tunnel\" obj=tunnels name=test_tunnel addr=http://localhost:425 url=https://12030abc.ngrok.io";
 			var expectedResult = new Dictionary<string, string>()
 			{
 				{ "t", "2020-03-01T23:36:39-0600" },
@@ -21,15 +21,15 @@ namespace Ngrok.AspNetCore.UnitTests
 				{ "addr", "http://localhost:425" },
 				{ "url", "https://12030abc.ngrok.io" }
 			};
-			var result = NgrokLogExtensions.ParseLogData(input);
+			var result = NgrokLogExtensions.ParseLogData(logData);
 
 			Assert.Equal(expectedResult, result);
 		}
 
 		[Fact]
-		public void ParseMisc()
+		public void ParseNoConfigSuppliedLogDataIntoDictionary()
 		{
-			var input = "t=2020-03-02T00:44:26-0600 lvl=info msg=\"no configuration paths supplied\"";
+			var logData = "t=2020-03-02T00:44:26-0600 lvl=info msg=\"no configuration paths supplied\"";
 			var expectedResult = new Dictionary<string, string>()
 			{
 				{ "t", "2020-03-02T00:44:26-0600" },
@@ -37,7 +37,7 @@ namespace Ngrok.AspNetCore.UnitTests
 				{ "msg", "no configuration paths supplied" }
 			};
 
-			var result = NgrokLogExtensions.ParseLogData(input);
+			var result = NgrokLogExtensions.ParseLogData(logData);
 
 			Assert.Equal(expectedResult, result);
 		}
